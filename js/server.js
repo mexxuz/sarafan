16cf5053a8791c82b359a330977f396544661c60
mexxuz <novuxodanosr@gmail.com>
2026-09-23 01:24:05 +0500

    Новый адрес сервера

--- js/server.js
+++ js/server.js
@@ -1,2 +1,2 @@
 ﻿// Адрес сервера. Его вписывает tools/start.ps1 — руками трогать не нужно.
-window.SARAFAN_SERVER = 'https://gdp-gazette-undertaken-nearly.trycloudflare.com';
+window.SARAFAN_SERVER = 'https://lloyd-calculated-aim-existed.trycloudflare.com';
