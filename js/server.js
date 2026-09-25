b9b2eccc2ad394f2f16b4163873734f9021c94f3
mexxuz <novuxodanosr@gmail.com>
2026-09-25 12:55:31 +0500

    Новый адрес сервера

--- js/server.js
+++ js/server.js
@@ -1,2 +1,2 @@
 // Адрес сервера. Его вписывает сторож — руками трогать не нужно.
-window.SARAFAN_SERVER = 'https://api.trycloudflare.com';
+window.SARAFAN_SERVER = 'https://exclusive-holes-notify-til.trycloudflare.com';
